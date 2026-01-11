fa87ec4dadfc450a2a4cfea7f803f0ee189d6cd8
Husni <husniwfayo@gmail.com>
2026-01-11 22:22:31 +0530

    configured MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { connectDatabase } from './infrastructure/database';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (_, res) => {
     res.send('Hello from TypeScript + Express!');
 });
+
+connectDatabase()
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
