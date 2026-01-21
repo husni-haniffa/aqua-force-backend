df79bd700242083a6a4245071606783395ee4bd1
Husni <husniwfayo@gmail.com>
2026-01-21 17:57:56 +0530

    refactored form schema validations for better rules

--- src/domain/dtos/news.ts
+++ src/domain/dtos/news.ts
@@ -1,13 +1,15 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const createNewsDTO = z.object({
     title: z
-    .string()
-    .min(10, 'Title must be atleast 25 characters')
-    .max(10, 'Title must not be more than 50 characters'),
+        .string()
+        .trim()
+        .min(25, "Title must be at least 25 characters")
+        .max(50, "Title must not exceed 50 characters"),
 
     content: z
-    .string()
-    .min(10, 'Content must be atleast 75 characters')
-    .max(15, 'Content must not be more than 725 characters')
-})
+        .string()
+        .trim()
+        .min(75, "Content must be at least 75 characters")
+        .max(725, "Content must not exceed 725 characters"),
+});
