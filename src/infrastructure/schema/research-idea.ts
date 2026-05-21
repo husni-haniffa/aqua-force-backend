8bb4e1ce5a44d7859103c3d2808f25aee5f6d667
Husni <husniwfayo@gmail.com>
2026-05-21 17:45:05 +0530

    removed social media required

--- src/infrastructure/schema/research-idea.ts
+++ src/infrastructure/schema/research-idea.ts
@@ -13,19 +13,19 @@ const researchIdeaSchema = new mongoose.Schema(
             type: String,
             required: true,
             trim: true,
-            maxlength: 200
+            maxlength: 255
         },
 
         mobile: {
             type: String,
             required: true,
-            match: /^07\d{8}$/
+            match: /^\d{10}$/
         },
 
         whatsapp: {
             type: String,
             required: true,
-            match: /^07\d{8}$/
+            match: /^\d{10}$/
         },
 
         email: {
@@ -36,13 +36,11 @@ const researchIdeaSchema = new mongoose.Schema(
         },
 
         linkedin: {
-            required: true,
             type: String,
             trim: true
         },
 
         orcid: {
-            required: true,
             type: String,
             trim: true
         },
@@ -53,7 +51,6 @@ const researchIdeaSchema = new mongoose.Schema(
         },
 
         scholar: {
-            required: true,
             type: String,
             trim: true
         },
