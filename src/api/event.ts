86057a487fb5c6d44e296ab0582bf5523a3ac68e
Husni <husniwfayo@gmail.com>
2026-06-01 17:08:57 +0530

    used seperate api to fetch active events for public and admins

--- src/api/event.ts
+++ src/api/event.ts
@@ -1,12 +1,13 @@
 import express from 'express'
-import { createEvent, getAllEvents, getEventById, updateEvent, deleteEvent } from '../applications/events'
+import { createEvent, getAllEvents, getEventById, updateEvent, deleteEvent, getActiveEvents } from '../applications/events'
 import { requireAuth, requireAdmin } from '../domain/middleware'
 import { imageUpload } from '../applications/events/helper'
 
 const eventRouter = express.Router()
 
 eventRouter.route('/').post(requireAuth, requireAdmin, imageUpload.single('file'), createEvent)
-eventRouter.route('/').get(getAllEvents)
+eventRouter.route('/').get(requireAuth, requireAdmin, getAllEvents)
+eventRouter.route('/all').get(getActiveEvents)
 eventRouter.route('/:id').get(getEventById)
 eventRouter.route('/:id').put(requireAuth, requireAdmin, imageUpload.single('file'), updateEvent)
 eventRouter.route('/:id').delete(requireAuth, requireAdmin, deleteEvent)
--- src/applications/events/get.ts
+++ src/applications/events/get.ts
@@ -8,8 +8,28 @@ export const getAllEvents = async (
     next: NextFunction
 ) => {
     try {
+        const response = await Event.find().sort({ createdAt: -1 });
+
+        const events = formatTimestamps(response);
+
+        res.status(200).json({
+            statusCode: 200,
+            message: "Events retrieved successfully",
+            data: events,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getActiveEvents = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
         const today = new Date();
-        today.setHours(0, 0, 0, 0); // start of today
+        today.setHours(0, 0, 0, 0);
 
         const response = await Event.find({
             eventDate: { $gte: today }
@@ -19,7 +39,7 @@ export const getAllEvents = async (
 
         res.status(200).json({
             statusCode: 200,
-            message: "Events retrieved successfully",
+            message: "Active events retrieved successfully",
             data: events,
         });
     } catch (error) {
