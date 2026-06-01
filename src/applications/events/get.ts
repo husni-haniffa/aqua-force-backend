bc730959a1a20866753a50ee637f31865c6ed311
Husni <husniwfayo@gmail.com>
2026-06-01 14:41:59 +0530

    events will be fetched only future dates

--- src/applications/events/get.ts
+++ src/applications/events/get.ts
@@ -8,13 +8,21 @@ export const getAllEvents = async (
     next: NextFunction
 ) => {
     try {
-        const response = await Event.find().sort({ createdAt: -1 })
-        const events = formatTimestamps(response)
+        const today = new Date();
+        today.setHours(0, 0, 0, 0); // start of today
+
+        const response = await Event.find({
+            eventDate: { $gte: today }
+        }).sort({ eventDate: 1 });
+
+        const events = formatTimestamps(response);
+
         res.status(200).json({
             statusCode: 200,
-            message: "Events retreived successfully", data: events
-        })
+            message: "Events retrieved successfully",
+            data: events,
+        });
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
