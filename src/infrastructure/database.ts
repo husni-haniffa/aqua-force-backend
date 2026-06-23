2cc7202612b2fa9675c147704f4efab2df80f721
Husni <husniwfayo@gmail.com>
2026-06-23 15:35:45 +0530

    updated

--- src/infrastructure/database.ts
+++ src/infrastructure/database.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 export const connectDatabase = async () => {
     try {
-        if (!process.env.MONGO_URI) {
+        if (!process.env.MONGODB_URI) {
             throw new AppError("MONGO_URI is not defined",500);
         }
 
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
 
     } catch (error) {
         console.error("Database connection failed:", error);
