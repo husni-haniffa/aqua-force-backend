4286b0feca3ae0ca428462a9586fd9cae674145e
Husni <husniwfayo@gmail.com>
2026-06-27 11:51:03 +0530

    added development hosting url

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,12 @@ connectDatabase()
 
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://research-minds-net-frontend.vercel.app", "https://www.researchmindsnet.com"],
+        origin: [
+            "http://localhost:3000", 
+            "https://research-minds-net-frontend.vercel.app", 
+            "https://www.researchmindsnet.com",
+            "https://researchmindsnetdev.netlify.app",
+        ],
     })
 );
 
