b166547aefe3eaf296a80599b7fcfc2469ef1af6
Husni <husniwfayo@gmail.com>
2026-07-09 15:07:50 +0530

    ensure requested user has submitted research to fetch data to edit

--- src/applications/submissions/user/getById.ts
+++ src/applications/submissions/user/getById.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { getSignedDownloadUrl } from "../helper";
 import Submission from "../../../infrastructure/schema/submission";
-import { NotFoundError } from "../../../domain/errors";
+import { NotFoundError, UnauthorizedError } from "../../../domain/errors";
+import { getAuth } from "@clerk/express";
 
 export const getSubmissionById = async (
     req: Request,
@@ -9,29 +10,34 @@ export const getSubmissionById = async (
     next: NextFunction
 ) => {
     try {
-        const id = req.params.id;
+        const { id } = req.params;
+        const { userId } = getAuth(req);
 
-        const submission = await Submission.findById(id).populate([
-            { path: 'categoryId', select: 'name' },
-            { path: 'researchTypeId', select: 'name' }
+        if (!userId) {
+            throw new UnauthorizedError();
+        }
+
+        const submission = await Submission.findOne({
+            _id: id,
+            userId,
+        }).populate([
+            { path: "categoryId", select: "name" },
+            { path: "researchTypeId", select: "name" },
         ]);
 
         if (!submission) {
-            throw new NotFoundError("Submission not found")
+            throw new NotFoundError("Submission not found");
         }
 
-        const filePath = await getSignedDownloadUrl(submission.filePath)
-
-        const response = {
-            ...submission.toObject(),
-            filePath,
-        }
+        const filePath = await getSignedDownloadUrl(submission.filePath);
 
         res.status(200).json({
             statusCode: 200,
-            data: response,
-        })
-
+            data: {
+                ...submission.toObject(),
+                filePath,
+            },
+        });
     } catch (error) {
         next(error);
     }
